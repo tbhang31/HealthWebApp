363a5e42d5e5ef2a29c093c48a695b03709d6950
Tom Hang <tbhang31@gmail.com>
2022-06-08 19:43:20 -0400

    I think we're done for now!

--- liftoffHealthCare/Controllers/HomeController.cs
+++ liftoffHealthCare/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace liftoffHealthCare.Controllers
             return View();
         }
 
+        public IActionResult Resources()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
